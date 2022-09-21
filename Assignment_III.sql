dbdb834ff1296bb3d7295b4185085252d8a11db1
Yapnounkie <edmundopierre@yahoo.com>
2022-09-20 21:42:51 -0700

    add assignment_III.sql

---
+++ Assignment_III.sql
@@ -0,0 +1,9 @@
+--Determine the Count of rows satisfying certain condition
+SELECT COUNT(*) FROM employee
+WHERE id = 2;
+
+--Determine average of a column
+SELECT AVG(amount) FROM customer;
+
+--Determine Sum of a column
+SELECT SUM(cabs) FROM cab_company;
